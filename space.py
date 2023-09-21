45b794fc53a0111aac8fd3cc8d27483c5de89526
ForzaBastia <145556386+ForzaBastia@users.noreply.github.com>
2023-09-21 18:04:16 +0200

    Add files via upload

--- space.py
+++ space.py
@@ -5,4 +5,9 @@ class Joueur() : # classe pour créer le vaisseau du joueur
     def __init__(self) :
         self.sens = "O"
         self.position = 400
-        self.image = pygame.image.load('background.png')
+        self.image = pygame.image.load('vaisseau.png')
+    def deplacer(self):
+        if self.sens=='gauche' and self.position!=0:
+            self.position-=0.5
+        elif self.sens=='droite' and self.position!=744:
+            self.position+=0.5
