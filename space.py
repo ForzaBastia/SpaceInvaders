49d9edc2c6dd3fd08ff529997141f1c2eb907357
ForzaBastia <145556386+ForzaBastia@users.noreply.github.com>
2023-09-21 17:33:49 +0200

    Add files via upload

---
+++ space.py
@@ -0,0 +1,8 @@
+import pygame  # necessaire pour charger les images et les sons
+
+
+class Joueur() : # classe pour créer le vaisseau du joueur
+    def __init__(self) :
+        self.sens = "O"
+        self.position = 400
+        self.image = pygame.image.load('background.png')
