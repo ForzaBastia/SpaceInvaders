d5ba9f8f9a9fd8dcc9d050920c2dccb461536c6b
ForzaBastia <145556386+ForzaBastia@users.noreply.github.com>
2023-10-03 01:30:13 +0200

    Add files via upload

--- space.py
+++ space.py
@@ -9,70 +9,120 @@ class Joueur() : # classe pour créer le vaisseau du joueur
         self.position = 330
         self.image = pygame.image.load('vaisseau.png')
         self.score = 0
+        self.hauteur = 470
+        self.point_vie = 3
     
     def deplacer(self) :
         if self.sens=='gauche' and self.position!=0 :
-            self.position-=5
+            self.position-=10
         elif self.sens=='droite' and self.position!=735 :
-            self.position+=5
+            self.position+=10
 
     def marquer(self) :
         self.score += 1
-        
+    
+    def perte_de_vie(self,coeur):
+        self.point_vie-=1
+        coeur.perdre_vie()
+        return True
     
     def tirer(self) :
         pass
-
-
+    
 class Balle() :
     
     def __init__(self, tireur) :
         self.tireur = tireur
-        self.depart = tireur.position + 48
+        self.depart = tireur.position + 48  # Stockez la position initiale
         self.hauteur = 492
         self.image = pygame.image.load('balle.png')
         self.etat = "chargee"
-        self.vitesse = 10
-    
+        self.vitesse = 30
+
     def bouger(self) :
         if self.etat == "attente" :
-            self.depart = self.tireur.position + 30
+            self.depart = self.tireur.position + 48
             self.hauteur = 492
         elif self.etat == "tiree" :
             self.hauteur = self.hauteur - self.vitesse
         
         if self.hauteur < 0 :
             self.etat = "attente"
-
-    def toucher(self, vaisseau) :
-        if (math.fabs(self.hauteur - vaisseau.hauteur) < 40) and (math.fabs(self.depart - vaisseau.depart) < 40) :
+    
+    def toucher(self, vaisseau):
+        if (math.fabs(self.hauteur - vaisseau.hauteur) < 40) and (math.fabs(self.depart - vaisseau.depart) < 40) and self.etat=='tiree':
             self.etat = "attente"
+            vaisseau.reapparaitre()
             return True
 
 class Ennemi() :
     NbEnnemis = 6
     
     def __init__(self) :
+        self.reapparaitre()
         self.depart = random.randint(1,700)
         self.hauteur = 10
-        self.type = random.randint(1,2)
+        self.type = random.randint(1,6)
         if self.type == 1 :
             self.image = pygame.image.load("invader1.png")
-            self.vitesse = 0.1
+            self.vitesse = 1
         elif self.type == 2 :
             self.image = pygame.image.load("invader2.png")
-            self.vitesse = 0.2
+            self.vitesse = 1
+        elif self.type == 3 :
+            self.image = pygame.image.load("invader3.png")
+            self.vitesse = 1
+        elif self.type == 4 :
+            self.image = pygame.image.load("invader4.png")
+            self.vitesse = 1
+        elif self.type == 5 :
+            self.image = pygame.image.load("invader5.png")
+            self.vitesse = 1
+        elif self.type == 6 :
+            self.image = pygame.image.load("invader6.png")
+            self.vitesse = 1
             
     def avancer(self):
         self.hauteur = self.hauteur + self.vitesse
-
-    def disparaitre(self) :
+        if self.hauteur == 600 :
+            self.hauteur = 0
+        if self.hauteur > 700 :
+            self.respawn()
+    
+    def reapparaitre(self):
         self.depart = random.randint(1,700)
         self.hauteur = 10
-        self.type = random.randint(1,2)
+        self.type = random.randint(1,6)
         if self.type == 1 :
             self.image = pygame.image.load("invader1.png")
-            self.vitesse = 0.1
+            self.vitesse = 1
         elif self.type == 2 :
             self.image = pygame.image.load("invader2.png")
-            self.vitesse = 0.2
+            self.vitesse = 1
+        elif self.type == 3 :
+            self.image = pygame.image.load("invader3.png")
+            self.vitesse = 1
+        elif self.type == 4 :
+            self.image = pygame.image.load("invader4.png")
+            self.vitesse = 1
+        elif self.type == 5 :
+            self.image = pygame.image.load("invader5.png")
+            self.vitesse = 1
+        elif self.type == 6 :
+            self.image = pygame.image.load("invader6.png")
+            self.vitesse = 1
+    
+    def touchejoueur(self,joueur):
+        if (math.fabs(self.hauteur - joueur.hauteur) < 40) and (math.fabs(self.depart - joueur.position) < 40):
+            self.reapparaitre()
+            return True
+    
+class Point_de_vie():
+    def __init__(self, joueur):
+        self.joueur = joueur
+        self.position = 0
+        self.hauteur = 0
+        self.point_de_vie_image = pygame.image.load('coeur.png')
+
+    def perdre_vie(self):
+        self.position -= 200
--- space_invaders_5.py
+++ space_invaders_5.py
@@ -4,31 +4,42 @@ import sys # pour fermer correctement l'application
 
 # lancement des modules inclus dans pygame
 pygame.init() 
-
-# création d'une fenêtre de 800 par 600
 screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Space Invaders") 
+pygame.display.set_caption("Space Invaders")
+
 # chargement de l'image de fond
 fond = pygame.image.load('background.png')
+fin_screen = pygame.Surface((800,600))
+fin_fond = pygame.image.load('game_over.png')
+fin = False
 
 # creation du joueur
 player = space.Joueur()
+
 # creation de la balle
 tir = space.Balle(player)
 tir.etat = "chargee"
+# creation des points de vie
+listePv = []
+for indice in range(player.point_vie):
+    pv = space.Point_de_vie(player)
+    listePv.append(pv)
+
 # creation des ennemis
 listeEnnemis = []
 for indice in range(space.Ennemi.NbEnnemis):
     vaisseau = space.Ennemi()
     listeEnnemis.append(vaisseau)
-    
+
+# creation du score
+
 ### BOUCLE DE JEU  ###
 running = True # variable pour laisser la fenêtre ouverte
 
 while running : # boucle infinie pour laisser la fenêtre ouverte
     # dessin du fond
     screen.blit(fond,(0,0))
-
+    temps_actif = pygame.time.get_ticks()
     ### Gestion des événements  ###
     for event in pygame.event.get(): # parcours de tous les event pygame dans cette fenêtre
         if event.type == pygame.QUIT : # si l'événement est le clic sur la fermeture de la fenêtre
@@ -39,29 +50,54 @@ while running : # boucle infinie pour laisser la fenêtre ouverte
         if event.type == pygame.KEYDOWN : # si une touche a été tapée KEYUP quand on relache la touche
             if event.key == pygame.K_LEFT : # si la touche est la fleche gauche
                 player.sens = "gauche" # on déplace le vaisseau de 1 pixel sur la gauche
-            if event.key == pygame.K_RIGHT : # si la touche est la fleche droite
+            elif event.key == pygame.K_RIGHT : # si la touche est la fleche droite
                 player.sens = "droite" # on déplace le vaisseau de 1 pixel sur la gauche
-            if event.key == pygame.K_SPACE : # espace pour tirer
+            elif event.key == pygame.K_SPACE : # espace pour tirer
                 player.tirer()
                 tir.etat = "tiree"
-
+        else :
+            player.sens= "O"
+    if player.point_vie <= 0:
+        fin = True
+        screen.blit(fin_fond,(0, 0))
     ### Actualisation de la scene ###
     # Gestions des collisions
     for ennemi in listeEnnemis:
         if tir.toucher(ennemi):
-            ennemi.disparaitre()
             player.marquer()
-            print(f"Score = {player.score} points")
+            print(f"Score : {player.score} points")
+        if ennemi.touchejoueur(player)==True:
+            player.perte_de_vie(pv)
+            print(f'Point de vie : {player.point_vie}')
+            if len(listePv)==0:
+                listePv.pop(player.point_vie)
+    
+    font = pygame.font.SysFont("Pixeltype.ttf", 0)
+    score_display = font.render(f'Score : {player.score}', 1, (255,255,0))
+    screen.blit(score_display, (25, 25))
+    
     # placement des objets
     # le joueur
     player.deplacer()
-    screen.blit(tir.image,[tir.depart,tir.hauteur]) # appel de la fonction qui dessine le vaisseau du joueur        
+    screen.blit(tir.image,[tir.depart,tir.hauteur]) # appel de la fonction qui dessine le vaisseau du joueur
+    # les cœurs de vie
+    x = 0
+    for point_vie in listePv:
+        screen.blit(point_vie.point_de_vie_image,[point_vie.position + x, point_vie.hauteur])
+        x += 30
     # la balle
     tir.bouger()
-    screen.blit(player.image,[player.position,500]) # appel de la fonction qui dessine le vaisseau du joueur
+    screen.blit(player.image,[player.position,player.hauteur])# appel de la fonction qui dessine le vaisseau du joueur
     # les ennemis
     for ennemi in listeEnnemis:
         ennemi.avancer()
         screen.blit(ennemi.image,[ennemi.depart, ennemi.hauteur]) # appel de la fonction qui dessine le vaisseau du joueur
-        
+    if fin:
+        screen.blit(fin_fond, (0, 0))
+        font = pygame.font.SysFont("Pixeltype.ttf", 40)
+        fin_text = font.render("Game Over", 1, (255, 0, 0))
+        score_text = font.render(f'Score : {player.score}', 1, (255, 255, 0))
+        screen.blit(fin_text, (200, 250))
+        screen.blit(score_text, (200, 300))
+    
     pygame.display.update() # pour ajouter tout changement à l'écran
